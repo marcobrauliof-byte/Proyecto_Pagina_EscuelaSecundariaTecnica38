e4129c82b43a88327890c4d5b178a70954a14358
Proyecto_Escuela_Secundaria_38 <marcobrauliof@gmail.com>
2026-06-16 23:06:10 -0600

    Add files via upload

---
+++ abrir.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
+
+function cerrar() {
+    document.getElementById("menu").classList.remove("active");
+}
